657208523aa66a69463edef9ad6cc358da20d906
nefbao <106868573+nefbao@users.noreply.github.com>
2022-08-06 15:02:22 +0430

    styled navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 function Navbar() {
   return (
-    <nav>
-      {" "}
-      <ul>
-        {" "}
-        <li>
-          {" "}
-          <Link to="/">Home</Link>{" "}
-        </li>{" "}
-        <li>
-          {" "}
-          <Link to="/About">About</Link>{" "}
-        </li>{" "}
-        <li>
-          {" "}
-          <Link to="/Work">Work</Link>{" "}
-        </li>{" "}
-        <li>
-          {" "}
-          <Link to="/Contact">Contact</Link>{" "}
-        </li>{" "}
-      </ul>{" "}
-    </nav>
+    <div classNameName="Navbar">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light navigation-bar">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand"
+            href="/"
+            title="Homepage of Nahid Norouzi"
+          >
+            Nahid Norouzi
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse navbar-items" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/About">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Work">
+                  Work
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 export default Navbar;
